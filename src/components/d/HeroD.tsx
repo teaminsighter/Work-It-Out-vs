a4bfe287ffbe2e810f641cd5b7a858cf5d3d1d9d
Insighter Digital <teaminsighter@gmail.com>
2025-08-25 05:39:16 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Card className="g

--- src/components/d/HeroD.tsx
+++ src/components/d/HeroD.tsx
@@ -40,7 +40,7 @@ const gridPositions = [
     "md:row-start-1 md:col-start-2 md:row-span-2", // Home
     "md:row-start-3 md:col-start-2", // Liability
     "md:row-start-1 md:col-start-4", // Car
-    "md:row-start-2 md:col-start-4", // Education
+    "md-row-start-2 md:col-start-4", // Education
     "md:row-start-3 md:col-start-3", // Travel
     "md:row-start-1 md:col-start-3", // Health
   ];
@@ -93,23 +93,19 @@ const HeroD = () => {
                     onClick={() => onOptionSelect(option.value, option.nextStepId)}
                     className={cn(
                       'group flex flex-col items-center justify-center text-center p-2 md:p-3 aspect-square rounded-xl shadow-lg transition-all duration-300 cursor-pointer hover:-translate-y-1 hover:shadow-2xl',
-                      option.value === 'home'
-                        ? 'bg-teal-400 text-white'
-                        : 'bg-white hover:bg-gray-50'
+                      'bg-white hover:bg-gray-50'
                     )}
                   >
                     <div
                       className={cn(
                         'p-1 md:p-1.5 rounded-md mb-1 transition-colors',
-                        option.value === 'home'
-                          ? 'bg-white/20'
-                          : 'bg-teal-100/30 group-hover:bg-teal-100/60'
+                        'bg-teal-100/30 group-hover:bg-teal-100/60'
                       )}
                     >
                       <option.icon
                         className={cn(
                           'h-3 w-3 md:h-4 md:w-4 transition-colors',
-                          option.value === 'home' ? 'text-white' : 'text-teal-500'
+                           'text-teal-500'
                         )}
                       />
                     </div>
